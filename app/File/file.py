892e15177f80bf90070277c725c7cec26b2bf3f0
MiguelCuellarR <miguel.1701823659@ucaldas.edu.co>
2023-11-02 15:25:17 -0500

    Reading text file with commas

--- app/File/file.py
+++ app/File/file.py
@@ -1,14 +1,15 @@
-from typing import List
-
-
-class File():
+class File:
 
     def uploadMap(self) -> list[list[str]]:
         map_data = []
-        with open("mundo.txt") as txt_file:
+        with open('resources/worlds/world.txt') as txt_file:
             for line in txt_file:
-                map_data.append(line.strip().split())
+                char = line.strip().replace(',', '').replace('\t', '')
+                char = char.split()
+                map_data.append(char)
         return map_data
 
     def saveMap(self) -> str:
         pass
+
+File.uploadMap(self=None)
