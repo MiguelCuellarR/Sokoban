7ecfe190436ee918741cde8b6449e1084831d7c4
james rojas <jamesfr8764@hotmail.com>
2023-12-16 13:09:54 -0500

    changes

--- app/behaviors/routes/uninformed/breadth.py
+++ app/behaviors/routes/uninformed/breadth.py
@@ -10,12 +10,13 @@ class Breadth(Route):
 
     def search(self):
         self.queue.append((self.root, ()))
+        level = 0
 
         while self.queue:
             vertex = self.queue.popleft()
             posV = vertex[0][0]
             typeV = vertex[0][1]
-            codeV = vertex[0][2]
+            codeV = vertex[0][2] 
             previousV = vertex[1]
 
             if vertex[0] == self.destiny:
@@ -29,11 +30,15 @@ class Breadth(Route):
                 adjList = self.graph[vertex[0]]
                 order = self.priority.priorityOrder(adjList)
 
+                if len(adjList) > 0:
+                    level = level + 1
+
                 for prio in order:
                     posN = prio[0]
                     typeN = prio[2]
                     codeN = prio[3]
                     if posN not in self.visited:
                         self.queue.append(((posN, typeN, codeN), posV))
+        print(level)
 
         return self.auxList
